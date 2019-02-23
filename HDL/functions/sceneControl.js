23da04bd0c2492a181a971a55f701fd45cea2a10
Hayden <haydendonald@gmail.com>
2019-02-23 12:05:04 +1100

    Changed the reporting of bad parameters

--- HDL/functions/sceneControl.js
+++ HDL/functions/sceneControl.js
@@ -21,10 +21,10 @@ module.exports = {
                 }
             },
             generateData: function(data) {
-                if (typeof data.areaNumber != 'number'){ return "Invalid area number: " + data.areaNumber; }
-                if (typeof data.sceneNumber != 'number'){ return "Invalid scene number: " + data.sceneNumber; }
-                if(data.areaNumber < 1 || data.areaNumber > 255){return "Invalid area number: " + data.areaNumber + ". This is expected to be a number between 1 and 255"}
-                if(data.sceneNumber < 0 || data.sceneNumber > 255){return "Invalid scene number: " + data.sceneNumber + ". This is expected to be a number between 0 and 255"}
+                if (typeof data.areaNumber != 'number'){return "Invalid area number: " + data.areaNumber + ". This is expected to be a number between 1 and 255";}
+                if (typeof data.sceneNumber != 'number'){return "Invalid scene number: " + data.sceneNumber + ". This is expected to be a number between 0 and 255";}
+                if(data.areaNumber < 1 || data.areaNumber > 255){return "Invalid area number: " + data.areaNumber + ". This is expected to be a number between 1 and 255";}
+                if(data.sceneNumber < 0 || data.sceneNumber > 255){return "Invalid scene number: " + data.sceneNumber + ". This is expected to be a number between 0 and 255";}
                 return Buffer.from([data.areaNumber, data.sceneNumber]);
             }
         },
@@ -39,7 +39,7 @@ module.exports = {
                 }
             },
             generateData: function(data) {
-                if (typeof data.areaNumber != 'number'){ return "Invalid area number: " + data.areaNumber; }
+                if (typeof data.areaNumber != 'number'){return "Invalid area number: " + data.areaNumber + ". This is expected to be a number between 1 and 255";}
                 if(data.areaNumber < 1 || data.areaNumber > 255){return "Invalid area number: " + data.areaNumber + ". This is expected to be a number between 1 and 255"}
                 return Buffer.from([data.areaNumber]);
             }
